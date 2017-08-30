ad4e02b26a96508ad0fd4dba24878127fec7ad55
Tajhna McCourtie <tmc253@hotmail.com>
2017-08-30 17:59:11 -0400

    created the sign up details

--- signup.php
+++ signup.php
@@ -2,20 +2,55 @@
 <html lang="en">
     <head>
         <title>Corona Account</title>
+        <link rel="stylesheet" href="css/signup.css">
     </head>
     <body>
         <div id="head">
         <h3>Create An Account</h3>
         <p>Please fill out all fields below</p>
         </div>
-        <div id="signup info">
+        <div id="signup-info">
             <form>
-                <label>First Name</label>
-                <input type="text" name="first name">
-                <label>Last Name</label>
-                <input type="text" name="last name">
-                <label>Gender</label>
-                <
+                <div id="fname">
+                  <label>First Name</label>
+                  <input type="text" name="first name">
+                </div>
+
+                  <div id="lname">
+                    <label>Last Name</label>
+                    <input type="text" name="last name">
+                  </div>
+
+                   <div id="gen">
+                     <label>Gender</label>
+                     <select name="gender">
+                      <option value="Female">Female</option>
+                        <option value="Male" selected>Male</option>
+                     </select>
+                  </div>
+
+                    <div id="emailadd">
+                       <label>Email</label>
+                       <input type="email" name="email-address">
+                    </div>
+                    
+                    <div id="pword">
+                      <label>Password</label>
+                      <input type="password" name="password">
+                    </div>
+
+                       <div id="altemail">
+                           <label>Alternate Email</label>
+                           <input type="email" name="email2">
+                       </div>
+
+                       <div id="btn">
+                           <button name="submit">Create Account</button>
+                       </div>
+
+
+
+
             </form>
         <div>
     </body>
