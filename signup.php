4e56568eab11df13b4c737422c4f198a5422d60b
Tajhna McCourtie <tmc253@hotmail.com>
2017-08-30 21:38:47 -0400

    added design to sign up page

--- signup.php
+++ signup.php
@@ -9,49 +9,65 @@
         <h3>Create An Account</h3>
         <p>Please fill out all fields below</p>
         </div>
-        <div id="signup-info">
+        <div class="container">
             <form>
-                <div id="fname">
+                <div class="form-input" id="fname">
                   <label>First Name</label>
+                </div>
+
+                <div class="form-input">
                   <input type="text" name="first name">
                 </div>
 
-                  <div id="lname">
+                  <div  class="form-input" id="lname">
                     <label>Last Name</label>
+                  </div>
+                     <div class="form-input">
                     <input type="text" name="last name">
                   </div>
 
-                   <div id="gen">
+                   <div class= "form-input" id="gen">
                      <label>Gender</label>
+                   </div>
+
+                     <div class="form-input">
                      <select name="gender">
                       <option value="Female">Female</option>
-                        <option value="Male" selected>Male</option>
+                        <option value="Male">Male</option>
                      </select>
                   </div>
 
-                    <div id="emailadd">
+                    <div class= "form-input" id="emailadd">
                        <label>Email</label>
+                    <div>
+                        <div class="form-input" >
                        <input type="email" name="email-address">
                     </div>
                     
-                    <div id="pword">
-                      <label>Password</label>
+                    <div class= "form-input" id="pword">
+                      <label>Password</label >
+                    </div>
+                      
+                      <div class="form-input">
                       <input type="password" name="password">
                     </div>
 
-                       <div id="altemail">
+                       <div class ="form-input" id="altemail">
                            <label>Alternate Email</label>
+                       </div>
+
+                       <div class="form-input">
                            <input type="email" name="email2">
                        </div>
 
-                       <div id="btn">
-                           <button name="submit">Create Account</button>
+                       <div class="btn-login">
+                           <input  type="submit" name="submit" value="Create Account">
                        </div>
 
 
 
 
             </form>
-        <div>
+        </div>
     </body>
 </html>
